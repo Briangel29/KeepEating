c3c46588479ba917fe066dd902d6370d0ae73074
briangel_29 <briangelhdz02@gmail.com>
2023-10-06 15:11:38 -0400

    Creating a shopping website

---
+++ KeepEating/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using KeepEating.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace KeepEating.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Category.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ KeepEating/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using KeepEating.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeepEating.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
